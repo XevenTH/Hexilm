4fce949e661f69edca8a8138cfba6dde6966477f
Angga <emmanuelangga@gmail.com>
2023-03-17 18:43:50 +0700

    Updating Property Create Profile

--- Model/UserApp.cs
+++ Model/UserApp.cs
@@ -4,7 +4,9 @@ namespace Model;
 
 public class UserApp : IdentityUser
 {
-    public string Displayname { get; set; } = string.Empty;
+    public string DisplayName { get; set; } = string.Empty;
     public string Bio { get; set; } = string.Empty;
+    public ICollection<FavoriteMovies> FavoriteMovies { get; set; } = new List<FavoriteMovies>();
     public ICollection<UserRoom> UserRooms { get; set; } = new List<UserRoom>();
+
 }
