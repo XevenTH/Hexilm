8a0f68734e7ab5cca45e7b0fa22e5859bc62c838
Angga <emmanuelangga@gmail.com>
2023-03-30 21:05:04 +0700

    Adding PhotosController and Modify Related Photo DTO

--- API/Controllers/DTO/UserDTO.cs
+++ API/Controllers/DTO/UserDTO.cs
@@ -4,6 +4,6 @@ public class UserDTO
 {
     public string Id { get; set; }
     public string DisplayName { get; set; }    
-    public string Username { get; set; }
+    public string UserName { get; set; }
     public string Token { get; set; }
 }
