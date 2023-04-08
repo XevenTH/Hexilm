cea561e687f31562ce9f49cd5acc07068cf412d8
Angga <emmanuelangga@gmail.com>
2023-04-08 23:20:13 +0700

    Adding Migrations UserRoom Entity and Updating Seeding Script

---
+++ Persistence/Migrations/20230408150300_ModifyUserRoomEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyUserRoomEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHost",
+                table: "UserRooms_Join",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsHost",
+                table: "UserRooms_Join");
+        }
+    }
+}
