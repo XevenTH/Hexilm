13e2b7eddad3970fef9ad0bb4c54c7a723a24ee1
Angga <emmanuelangga@gmail.com>
2023-04-15 13:17:36 +0700

    Modify Profile Model to Have 2 Overloading

--- Client/src/App/model/profile.ts
+++ Client/src/App/model/profile.ts
@@ -1,27 +1,30 @@
+import { Movie } from "./movie";
+import Photo from "./photo";
+
 export interface Profile {
   displayName: string;
   userName: string;
   bio: string;
-  favoriteMovies: [
-    {
-      id: string;
-      title: string;
-      description: string;
-      picture: string;
-    }
-  ];
+  photos: Photo[]
+  favoriteMovies: Movie[]
+}
+
+export class Profile implements Profile {
+  constructor(init?: InitialEditProfile) {
+    Object.assign(this, init);
+  }
 }
 
 export class InitialEditProfile {
-  displayName= "";
-  userName= "";
-  bio= "";
-  
+  displayName: string = '';
+  userName: string = '';
+  bio: string = '';
+
   constructor(request?: InitialEditProfile) {
     if (request) {
       this.displayName = request.displayName;
-    this.userName = request.userName;
-    this.bio = request.bio;
-    }    
+      this.userName = request.userName;
+      this.bio = request.bio;
+    }
   }
 }
