939cf20564fcb2b625bbedd73516fbb755d3585b
Angga <emmanuelangga@gmail.com>
2023-04-17 12:24:02 +0700

    Deleting Picture Property To not Having Picture Property

--- Application/Movies/DTO/MiniMovieDTO.cs
+++ Application/Movies/DTO/MiniMovieDTO.cs
@@ -5,5 +5,4 @@ public class MiniMovieDto
     public Guid Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public string Picture { get; set; } = string.Empty;
 }
