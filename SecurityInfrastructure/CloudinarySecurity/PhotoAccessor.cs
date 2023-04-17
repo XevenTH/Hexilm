d1793acbeedaaedd25e878b97bd37ca68ce3002c
Angga <emmanuelangga@gmail.com>
2023-04-17 12:16:54 +0700

    Updating PhotoAccessor To have Movie Landscape and Potrait Upload Image

--- SecurityInfrastructure/CloudinarySecurity/PhotoAccessor.cs
+++ SecurityInfrastructure/CloudinarySecurity/PhotoAccessor.cs
@@ -22,30 +22,76 @@ public class PhotoAccessor : IPhotoAccessor
         _cloudinary = new Cloudinary(account);
     }
 
-    public async Task<PhotoResult> UploadPhoto(IFormFile file)
+    public async Task<PhotoResult> UserProfileUploadPhoto500X500(IFormFile file)
     {
-        if (file.Length > 0)
+        if (file.Length <= 0) return null;
+        
+        await using var readFile = file.OpenReadStream();
+
+        var uploadParams = new ImageUploadParams()
         {
-            await using var readFile = file.OpenReadStream();
+            File = new FileDescription(file.Name, readFile),
+            Transformation = new Transformation().Height(500).Width(500).Crop("fill"),
+            Format = "webp"
+        };
 
-            var uploadParams = new ImageUploadParams()
-            {
-                File = new FileDescription(file.Name, readFile),
-                Transformation = new Transformation().Height(500).Width(500).Crop("fill")
-            };
+        var result = await _cloudinary.UploadAsync(uploadParams);
 
-            var result = await _cloudinary.UploadAsync(uploadParams);
+        if (result.Error != null) throw new Exception(result.Error.Message);
 
-            if (result.Error != null) throw new Exception(result.Error.Message);
+        return new PhotoResult()
+        {
+            PublicId = result.PublicId,
+            Url = result.SecureUrl.ToString()
+        };
+    }
 
-            return new PhotoResult()
-            {
-                PublicId = result.PublicId,
-                Url = result.SecureUrl.ToString()
-            };
-        }
+    public async Task<PhotoResult> MovieUploadPhotoLandscape(IFormFile file)
+    {
+        if (file.Length <= 0) return null;
+        
+        await using var readFile = file.OpenReadStream();
 
-        return null;
+        var uploadParams = new ImageUploadParams()
+        {
+            File = new FileDescription(file.Name, readFile),
+            Transformation = new Transformation().Height(1080).Width(1440).Crop("fill"),
+            Format = "webp"
+        };
+
+        var uploadResult = await _cloudinary.UploadAsync(uploadParams);
+
+        if (uploadResult.Error != null) throw new Exception(uploadResult.Error.Message);
+
+        return new PhotoResult()
+        {
+            PublicId = uploadResult.PublicId,
+            Url = uploadResult.Url.ToString()
+        };
+    }
+
+    public async Task<PhotoResult> MovieUploadPhotoPortrait(IFormFile file)
+    {
+        if (file.Length <= 0) return null;
+        
+        await using var readFile = file.OpenReadStream();
+
+        var uploadParams = new ImageUploadParams()
+        {
+            File = new FileDescription(file.Name, readFile),
+            Transformation = new Transformation().Height(1440).Width(1080).Crop("fill"),
+            Format = "webp"
+        };
+
+        var uploadResult = await _cloudinary.UploadAsync(uploadParams);
+
+        if (uploadResult.Error != null) throw new Exception(uploadResult.Error.Message);
+
+        return new PhotoResult()
+        {
+            PublicId = uploadResult.PublicId,
+            Url = uploadResult.Url.ToString()
+        };
     }
 
     public async Task<string> DeletePhoto(string publicId)
