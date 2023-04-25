4313a6541529b91890c61264224f4c5648ebb5e4
Angga <emmanuelangga@gmail.com>
2023-04-25 17:42:30 +0700

    FIx API Respond Bug FOr Responding Request and Error

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -15,9 +15,9 @@ public class BaseApiController : ControllerBase
     protected ActionResult GetResult<T>(ResultValidator<T> result)
     {
         if (result.IsSuccess && result.Value != null && result.StatusCode == 200) return Ok(result.Value);
-        if (!result.IsSuccess && result.Value == null && result.StatusCode == 404) return NotFound(CreateResponse(Response.StatusCode, result.Message));
+        if (!result.IsSuccess && result.Value == null && result.StatusCode == 404) return NotFound(CreateResponse(result.StatusCode, result.Message));
 
-        return BadRequest(CreateResponse(Response.StatusCode, result.Message));
+        return BadRequest(CreateResponse(result.StatusCode, result.Message));
     }
 
     protected object CreateResponse(int code, string message)
