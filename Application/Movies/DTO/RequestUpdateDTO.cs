212e3152b56d0dd0fe33a2e1030562fc70d0736c
Angga <emmanuelangga@gmail.com>
2023-04-25 18:33:54 +0700

    Adding DTO For Request Movie Update Schema

---
+++ Application/Movies/DTO/RequestUpdateDTO.cs
@@ -0,0 +1,7 @@
+namespace Application.Movies.DTO;
+
+public class RequestUpdateDTO
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;   
+}
