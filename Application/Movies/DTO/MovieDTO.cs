0395e679bc11f4d3f5cd86c860ec010a27f69e69
Angga <emmanuelangga@gmail.com>
2023-04-27 15:52:18 +0700

    Updating DTO To Have Collection Of Photos

--- Application/Movies/DTO/MovieDTO.cs
+++ Application/Movies/DTO/MovieDTO.cs
@@ -7,7 +7,7 @@ public class MovieDTO
     public Guid Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public string Picture { get; set; } = string.Empty;
-    public ICollection<Actor> Actors { get; set; } = new List<Actor>();
+    public ICollection<Photo> Photos { get; set; } = new List<Photo>();
+    public ICollection<ActorDTO> Actors { get; set; } = new List<ActorDTO>();
     public DirectorDTO Director { get; set; }
 }
