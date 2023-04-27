1901e0f63af049c633f8a8592a2042552f446a5b
Angga <emmanuelangga@gmail.com>
2023-04-27 15:51:34 +0700

    Updating DTO To Have Photo Property

--- Application/Movies/DTO/ActorDTO.cs
+++ Application/Movies/DTO/ActorDTO.cs
@@ -1,7 +1,10 @@
+using Model;
+
 namespace Application.Movies.DTO;
 
 public class ActorDTO
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public Photo Photo { get; set; }
 }
